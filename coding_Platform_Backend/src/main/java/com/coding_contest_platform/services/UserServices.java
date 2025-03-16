0edc05539871665aa1fbf0708b3e71e848d41c0b
coderX4 <104690831+coderX4@users.noreply.github.com>
2025-03-16 21:57:31 +0530

    the firstuser fetch implement

--- coding_Platform_Backend/src/main/java/com/coding_contest_platform/services/UserServices.java
+++ coding_Platform_Backend/src/main/java/com/coding_contest_platform/services/UserServices.java
@@ -1,7 +1,11 @@
 package com.coding_contest_platform.services;
 
 import com.coding_contest_platform.entity.User;
+import java.util.List;
+
 
 public interface UserServices {
     User saveUsers(User user);
+
+    List<User> getAllUsers();
 }
